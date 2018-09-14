333dd5d89ebcfe9a3fde9be58bc5dcd5d3b34157
Lintrail Dukes <dukes146@gmail.com>
2018-09-14 11:49:55 -0500

    Titanic data cleaning

---
+++ titanic_code.R
@@ -0,0 +1,22 @@
+titanic_df <- titanic_original
+head(titanic_df)
+
+#load dplyr, tidyr
+library(dplyr)
+library(tidyr)
+
+#Replace empty observations in Embark with S
+titanic_df[titanic_df$embarked == "", "embarked"] <- "S"
+
+
+# replace NA is age with mean of age
+titanic_df$age[is.na(titanic_df$age)] <- mean(titanic_df$age, na = TRUE)
+
+# fill empty boat values with "NA"
+titanic_df[titanic_df$boat == "", "boat" ] <- "NA"
+
+# create new column has_cabin_number
+titanic_df <- titanic_df %>%
+  mutate(has_cabin_number =ifelse(cabin == "", 1, 0))
+
+write.csv(titanic_df, file = "titanic_clean.csv")
